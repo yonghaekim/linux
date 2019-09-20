88622707f72ef05d7768e4165ab91cd892fd9d97
Nathan Pemberton <nathanp@berkeley.edu>
2019-09-20 20:23:51 +0000

    Read timebase from top of device tree to match Rocket behavior.

--- arch/riscv/kernel/time.c
+++ arch/riscv/kernel/time.c
@@ -18,8 +18,12 @@ void __init time_init(void)
 	u32 prop;
 
 	cpu = of_find_node_by_path("/cpus");
-	if (!cpu || of_property_read_u32(cpu, "timebase-frequency", &prop))
-		panic(KERN_WARNING "RISC-V system with no 'timebase-frequency' in DTS\n");
+	if (!cpu || of_property_read_u32(cpu, "timebase-frequency", &prop)) {
+		cpu = of_find_node_by_path("/cpus/cpu@0");
+		if (!cpu || of_property_read_u32(cpu, "timebase-frequency", &prop)) {
+			panic(KERN_WARNING "RISC-V system with no 'timebase-frequency' in DTS\n");
+		}
+	}
 	of_node_put(cpu);
 	riscv_timebase = prop;
 
