a3f46153197e2962502cd93a49f2d8328aaa00f5
Albert Ou <aou@eecs.berkeley.edu>
2019-10-02 01:16:02 +0000

    riscv: Fence before context switch
    Ensure that RoCC memory operations complete prior modifying the satp
    CSR, which is shared between the processor and RoCC accelerators.

--- arch/riscv/mm/context.c
+++ arch/riscv/mm/context.c
@@ -217,6 +217,14 @@ static void set_mm_noasid(struct mm_struct *mm)
 
 static inline void set_mm(struct mm_struct *mm, unsigned int cpu)
 {
+#ifdef CONFIG_RISCV_ROCC
+	/*
+	 * Fence to wait for RoCC memory operations to finish, since
+	 * satp is shared between the processor and RoCC accelerators.
+	 */
+	mb();
+#endif
+
 	if (static_branch_unlikely(&use_asid_allocator))
 		set_mm_asid(mm, cpu);
 	else
